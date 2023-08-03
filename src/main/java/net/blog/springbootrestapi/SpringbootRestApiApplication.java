a2dd2b2ba2bd1a87d33e3ab51dddd1e3db667c5d
mohammed ziyad <mohammedziyad@mohammeds-MacBook-Air.local>
2023-08-03 11:36:22 +0530

    changed app to azure

--- src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
+++ src/main/java/net/blog/springbootrestapi/SpringbootRestApiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 
+
 import javax.management.relation.Role;
 
 @SpringBootApplication
